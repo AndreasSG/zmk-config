42154cb3e866c666832c95fbcdfa4691e8e58585
Andreas H <andreas.hogberg5@gmail.com>
2024-10-23 10:39:58 +0200

    Update swe_keys.h

--- config/swe_keys.h
+++ config/swe_keys.h
@@ -67,8 +67,8 @@
 #define SE_RPAR  LS(N9)      // )
 #define SE_LBKT  RA(N8)      // [
 #define SE_RBKT  RA(N9)      // ]
-#define SE_LBRC  LS(LC(8))   // for '{' (Option + 8)
-#define SE_RBRC  LS(LC(9))   // for '}' (Option + 9)
+#define SE_LBRC  LS(LA(8))   // for '{' (Option + 8)
+#define SE_RBRC  LS(LA(9))   // for '}' (Option + 9)
 #define SE_PLUS  MINUS       // +
 #define SE_MINUS FSLH        // -
 #define SE_EQUAL LS(N0)      // =
@@ -80,6 +80,6 @@
 #define SE_TILDE RA(RBKT)    // ~
 #define SE_LT    GRAVE       // for '<'
 #define SE_GT    LS(GRAVE)   // for '>'
-#define SE_BSLH  LS(LC(7))  // for '\' (Option + Shift + 7)
-#define SE_PIPE  LC(7)  // for '|' (Option + 7)
+#define SE_BSLH  LS(LA(7))  // for '\' (Option + Shift + 7)
+#define SE_PIPE  LA(7)  // for '|' (Option + 7)
 
